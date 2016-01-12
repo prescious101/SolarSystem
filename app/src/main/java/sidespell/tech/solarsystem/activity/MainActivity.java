239dba833b27b678380442158c51b8e367ddf2e5
Prescious Alago <frederickalago@gmail.com>
2016-01-12 11:00:24 -0800

    Final Implementation of Solar System

--- app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -3,16 +3,22 @@ package sidespell.tech.solarsystem.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import sidespell.tech.solarsystem.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-
+    Spinner mSpin;
+    ImageView imageView;
+    int[] images = {R.mipmap.mercury,R.mipmap.venus,R.mipmap.earth,R.mipmap.mars,R.mipmap.jupiter,R.mipmap.saturn
+                    ,R.mipmap.uranus,R.mipmap.neptune};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageView = (ImageView) findViewById(R.id.image);
+        mSpin = (Spinner) findViewById(R.id.spnPlanet);
+
+        ArrayAdapter adapter= ArrayAdapter.createFromResource(this,R.array.Planets,android.R.layout.simple_spinner_item);
+
+        mSpin.setAdapter(adapter);
+        //this.mSpin.setSelection(0);
+        mSpin.setOnItemSelectedListener(this);
+
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //ImageView mImg
+        TextView mText = (TextView) view;
+        if(images!=null) {
+            imageView.setImageResource(images[position]);
+            Toast.makeText(this, "You Selected " + mText.getText() + " the position in array index is " + position, Toast.LENGTH_LONG).show();
+        }
+        else
+            Toast.makeText(this,"You Selected "+ mText.getText()+"position in index is"+position,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+
+
+
+
 }
